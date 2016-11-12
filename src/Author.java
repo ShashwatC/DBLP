db78484ca4b65e1b9eda08115f13e4343786a711
ShashwatC <shashwat15091@iiitd.ac.in>
2016-11-12 15:22:33 +0530

    Author instance variables added

--- src/Author.java
+++ src/Author.java
@@ -1,5 +1,13 @@
+import java.util.Collection;
+import java.util.List;
 
 public class Author {
+	String primaryName;
+	List<String> otherNames;
+	String title;
+	String url;
+	String additionalNotes;
+	Collection<Publication> papers;
 
 	public Author() {
 		// TODO Auto-generated constructor stub
