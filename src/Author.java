fbb8c30941b8629bae97746f26748bce0cacf350
ShashwatC <shashwat15091@iiitd.ac.in>
2016-11-12 15:30:45 +0530

    Added getters and setters, might need to remove some of them later

--- src/Author.java
+++ src/Author.java
@@ -2,15 +2,64 @@ import java.util.Collection;
 import java.util.List;
 
 public class Author {
-	String primaryName;
-	List<String> otherNames;
-	String title;
-	String url;
-	String additionalNotes;
-	Collection<Publication> papers;
-
+	private String primaryName;
+	private List<String> otherNames;
+	private String title;
+	private String url;
+	private String additionalNotes;
+	private Collection<Publication> papers;
+	
 	public Author() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public String getPrimaryName() {
+		return primaryName;
+	}
+
+	public void setPrimaryName(String primaryName) {
+		this.primaryName = primaryName;
+	}
+
+	public List<String> getOtherNames() {
+		return otherNames;
+	}
+
+	public void setOtherNames(List<String> otherNames) {
+		this.otherNames = otherNames;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getAdditionalNotes() {
+		return additionalNotes;
+	}
+
+	public void setAdditionalNotes(String additionalNotes) {
+		this.additionalNotes = additionalNotes;
+	}
+
+	public Collection<Publication> getPapers() {
+		return papers;
+	}
+
+	public void setPapers(Collection<Publication> papers) {
+		this.papers = papers;
+	}
+
+	
 }
--- src/Publication.java
+++ src/Publication.java
@@ -1,8 +1,91 @@
+import java.util.Date;
+import java.util.List;
 
 public class Publication {
-
+	private int key;
+	private Date mDate;
+	private String title;
+	private List<Author> authorList;
+	private int numPages;
+	private int year;
+	private int volume;
+	private String journalBook;
+	private String url;
+	
 	public Publication() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public Date getmDate() {
+		return mDate;
+	}
+
+	public void setmDate(Date mDate) {
+		this.mDate = mDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Author> getAuthorList() {
+		return authorList;
+	}
+
+	public void setAuthorList(List<Author> authorList) {
+		this.authorList = authorList;
+	}
+
+	public int getNumPages() {
+		return numPages;
+	}
+
+	public void setNumPages(int numPages) {
+		this.numPages = numPages;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public String getJournalBook() {
+		return journalBook;
+	}
+
+	public void setJournalBook(String journalBook) {
+		this.journalBook = journalBook;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
 	}
 
 }
