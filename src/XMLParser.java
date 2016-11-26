f991526c6f534ae71a30f8f8fb692267852fa704
ShashwatC <shashwat15091@iiitd.ac.in>
2016-11-27 02:35:56 +0530

    Parser strucure in compliance with SAX norms

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -1,7 +1,12 @@
 import java.io.File;
 import java.util.Collection;
 
-public class XMLParser {
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class XMLParser extends DefaultHandler {
 	File xmlInput;
 	Collection<Publication> publications;
 	Collection<Author> author;
@@ -10,6 +15,34 @@ public class XMLParser {
 		this.xmlInput = xmlInput;
 	}
 	
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes){
+		
+	}
+	
+	@Override
+	public void endElement(String uri, String localName, String qName){
+		
+	}
+
+	@Override
+	public void characters(char[] ch, int start, int length){
+		
+	}
+
+	@Override
+    public void warning(SAXParseException exception) throws SAXException {
+        throw new SAXException("Parser threw a Warning");
+    }
+
+	@Override
+    public void error(SAXParseException exception) throws SAXException {
+        throw new SAXException("Parser threw an Error");
+    }
+    
+    // No need to Override fatalError since it's implementation in superclass is
+    // exactly what we desire, i.e. the program should halt in case of fatal error.
+
 	Collection<Publication> getDBCollection(){
 		return publications;
 	}
