f81649247fdbfd96181f90cbe58b8299d9b4c8af
RadhikaG <radhikaghosal@gmail.com>
2016-11-27 19:09:29 +0530

    gui: work started

--- src/DBLP/SearchEngine.java
+++ src/DBLP/SearchEngine.java
@@ -1,20 +1,57 @@
 package DBLP;
 
+import java.awt.Component;
 import java.awt.Container;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class SearchEngine {
+/** \class SearchEngine
+ *  \brief Overall GUI for application
+ *  
+ *  Contains Header, QueryInputPanel, QueryOutputPanel
+ */
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		JFrame homeScreen = new JFrame("DBLP Search Engine");
-		homeScreen.setSize(300,300);
-		homeScreen.setLocationRelativeTo(null);
-		homeScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container pane = homeScreen.getContentPane();
-		homeScreen.setVisible(true);
-		
-	}
+public class SearchEngine extends JFrame {
+    
+    private final Font pFont = new Font("Arial", Font.PLAIN, 13);
+    private final int frameW = 800;
+    private final int frameH = 600;
+    
+    public SearchEngine() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(frameW, frameH);
+        this.add(new Header());
+        this.add(new QueryInputPanel());
+        this.add(new QueryOutputPanel());
+    }
+    
+    public void redraw(Component pan) {
+        this.getContentPane().removeAll();
+        this.add(pan);
+        this.validate();
+        this.getContentPane().repaint();
+    }
 
+    /** \class Header
+     *  \brief Displays big-ass heading in window
+     */
+    public class Header extends JPanel {
+        
+    }
+    
+    /** \class QueryInputPanel
+     *  \brief Inputs various params for different kinds of queries
+     */
+    public class QueryInputPanel extends JPanel {
+        
+    }
+    
+    /** \class QueryOutputPanel
+     *  \brief Outputs search results in tabular form.
+     */
+    public class QueryOutputPanel extends JPanel {
+        
+    }
 }
